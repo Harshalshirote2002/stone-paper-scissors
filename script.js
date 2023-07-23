10e3ffe2d2794658070d968ad41190618db76ca2
Harshal Shirote <harshalshirote2002@gmail.com>
2023-07-23 19:22:00 +0530

    Add GUI with basic functionalities

--- script.js
+++ script.js
@@ -1,63 +1,103 @@
-function getComputerChoice(){
-    let decider = Math.floor(Math.random()*10);
-    if(decider<3.5){
+const cards = Array.from(document.querySelectorAll('.hand'));
+bottomBar = document.querySelector('.bottom-bar');
+humanScore = document.querySelector('.human');
+cpuScore = document.querySelector('.cpu');
+
+cards.forEach(card => card.addEventListener('mouseover', (e) => e.target.classList.add('highlighted-icon')));
+cards.forEach(card => card.addEventListener('mouseleave', (e) => e.target.classList.remove('highlighted-icon')));
+
+function getComputerChoice() {
+    let decider = Math.floor(Math.random() * 10);
+    if (decider < 3.5) {
         return "stone";
-    }else if(decider>=3.5 && decider<7){
+    } else if (decider >= 3.5 && decider < 7) {
         return "paper";
-    }else{
-        return "scissor";
+    } else {
+        return "scissors";
     }
 }
 
-function getPlayerChoice(){
-    const choice=prompt("Make your choice here:").toLowerCase();
-    console.log(`player has selected ${choice}`);                              
-    return choice;
-}
-
-function gameRound(playerSelection, computerSelection){
+function gameRound(playerSelection, computerSelection) {
     //return 1 if the player wins, -1 if he loses, 0 if it's a draw 
-    if(playerSelection==computerSelection){
+    if (playerSelection == computerSelection) {
         return 0;
     }
-    if(playerSelection=="stone" && computerSelection=="scissor"){
+    if (playerSelection == "stone" && computerSelection == "scissors") {
         return 1;
-    }else if(playerSelection=="paper" && computerSelection=="stone"){
+    } else if (playerSelection == "paper" && computerSelection == "stone") {
         return 1;
-    }else if(playerSelection=="scissor" && computerSelection=="paper"){
+    } else if (playerSelection == "scissors" && computerSelection == "paper") {
         return 1;
-    }else if(playerSelection=="stone" && computerSelection=="paper"){
+    } else if (playerSelection == "stone" && computerSelection == "paper") {
         return -1;
-    }else if(playerSelection=="paper" && computerSelection=="scissor"){
+    } else if (playerSelection == "paper" && computerSelection == "scissors") {
         return -1;
-    }else if(playerSelection=="scissor" && computerSelection=="stone"){
+    } else if (playerSelection == "scissors" && computerSelection == "stone") {
         return -1;
     }
 }
 
-function game(){
-    let result=0;
-
-    for(let i=0;i<5;i++){
-        playerSelection = getPlayerChoice();
-        computerSelection = getComputerChoice();
-        console.log(`computer has selected ${computerSelection}`);
-        if(gameRound(playerSelection, computerSelection)==0){
-            console.log("draw!");
-        }else if(gameRound(playerSelection, computerSelection)==1){
-            console.log("player beats computer");
-        }else{
-            console.log("computer beats player");
-        }
-        result+=gameRound(playerSelection, computerSelection);
-    }
-    if(result>0){
-        console.log(`The player beats computer by ${Math.abs(result)} points thus winning the game!.`);
-    }else if(result<0){
-        console.log(`The computer beats player by ${Math.abs(result)} points thus winning the game!.`);
+function playGame(e) {
+    roundNumber += 1;
+    playerSelection = e.target.id;
+    computerSelection = getComputerChoice();
+    if(computerSelectionOld==null){
+        cpuHand = document.querySelector(`.cpu-${computerSelection}`);
+        cpuHand.classList.add('highlighted-icon');
+        computerSelectionOld = computerSelection;
     }else{
-        console.log("it's a draw!");
+        cpuHand = document.querySelector(`.cpu-${computerSelectionOld}`);
+        cpuHand.classList.remove('highlighted-icon');
+        cpuHand = document.querySelector(`.cpu-${computerSelection}`);
+        cpuHand.classList.add('highlighted-icon');
+        computerSelectionOld = computerSelection;
+    }
+    
+    console.log(`player has selected ${playerSelection}`);
+    console.log(`computer has selected ${computerSelection}`);
+    if (gameRound(playerSelection, computerSelection) == 0) {
+        console.log("draw!");
+    } else if (gameRound(playerSelection, computerSelection) == 1) {
+        console.log("player beats computer");
+        score1 += 1;
+    } else {
+        console.log("computer beats player");
+        score2 += 1
+    }
+    humanScore.textContent = `Score: ${score1}`;
+    cpuScore.textContent = `Score: ${score2}`;
+
+    if(score1>score2){
+        bottomBar.textContent = `Player leads the game with ${score1-score2} points!`
+    }else if(score1<score2){
+        bottomBar.textContent = `computer leads game with ${score2-score1} points!`
+    }else{
+        bottomBar.textContent  = "It's currently a tie!";
+    }
+
+    if (roundNumber == 5) {
+        if (score1 > score2) {
+            bottomBar.textContent = `The player beats computer by ${score1 - score2} points thus winning the game!.`;
+            console.log(`The player beats computer by ${score1 - score2} points thus winning the game!.`);
+        } else if (score1 < score2) {
+            bottomBar.textContent = `The computer beats player by ${score2 - score1} points thus winning the game!.`;
+            console.log(`The computer beats player by ${score2 - score1} points thus winning the game!.`);
+        } else {
+            bottomBar.textContent = "it's a draw!"
+            console.log("it's a draw!");
+        }
+        
+        score1 = 0;
+        score2 = 0;
+        roundNumber = 0;
+        computerSelectionOld=null;
     }
 }
 
-// game();
+let score1 = 0;
+let score2 = 0;
+let roundNumber = 0;
+computerSelectionOld=null;
+cards.forEach(card => card.addEventListener('click', playGame));
+
+
